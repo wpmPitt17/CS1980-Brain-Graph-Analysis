601c877dddfe54478420365eb1a51adeced111b2
Wyatt <wpm17@pitt.edu>
2026-02-27 08:34:47 -0500

    General structure of sub-network analysis and data conversion created

---
+++ sncm.py
@@ -0,0 +1,11 @@
+# sncm.py
+# Author: Wyatt McMullen
+#
+# Script for analyzing sub_networks in control group, target group, and combined.
+
+"""
+Sub-Network Connectomes
+
+This is the primary script for transforming edge lists of processed fMRI data from subjects into 
+adjacency lists providing methods to construct these structures for analysis of subnetworks in brain connectomes.
+"""
